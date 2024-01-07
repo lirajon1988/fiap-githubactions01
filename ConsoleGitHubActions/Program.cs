64561d23c1af307cd197ddf78a2d0782a8188c53
lira.jonas88 <lira.jonas88@gmail.comm>
2024-01-06 21:26:52 -0300

    Subindo projeto

---
+++ ConsoleGitHubActions/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Estou subindo um projeto de exemplo para testes do GitHUbActions");
+Console.WriteLine("Jonas!!!!");
